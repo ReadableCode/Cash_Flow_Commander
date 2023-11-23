fe2609922d48ca23152a90f207332e02e4f68444
jason <jasonwchristiansen@gmail.com>
2023-11-22 21:13:43 -0600

    use relative paths

--- scripts/crontab_extractor.sh
+++ scripts/crontab_extractor.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Construct the file path using a relative path from the script's location
 HOSTNAME=$(hostname)
-FILENAME="${SCRIPT_DIR}/../triggers/crontab_extraction_${HOSTNAME}.txt"
+# Get the directory of the script
+script_dir=$(cd "$(dirname "$0")" && pwd)
+echo "script_dir: $script_dir"
 
-# Extract crontab to the file
-crontab -l > "$FILENAME"
+# Set the relative directory path based on the script's location
+directory_path=$(realpath "$script_dir/../triggers")
+echo "directory_path: $directory_path"
 
-# Log the completion with a timestamp
+FILENAME="crontab_extraction_${HOSTNAME}.txt"
+
+# Define save location
+save_path="${directory_path}/${FILENAME}"
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") Starting crontab extraction, storing in $save_path"
+
+crontab -l > "$save_path"
+
 echo "$(date +"%Y-%m-%d %H:%M:%S") Completed crontab extraction"
-
